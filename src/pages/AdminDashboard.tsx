436e5bfd4be5c80daa110716ed3b06654e56b993
Lewis' Mind Hub <ephesianslewis@gmail.com>
2025-09-01 10:19:56 +0300

    admin dashboard

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -179,14 +179,7 @@ const AdminDashboard = () => {
             <div className="flex items-center space-x-2 sm:space-x-4 min-w-0">
               <div className="p-1.5 sm:p-2 rounded-lg flex items-center">
                 <img src={chietaLogo} alt="Chieta Logo" className="h-10 w-20 mr-2" />
-{/*                 <Settings className="h-5 w-5 sm:h-6 sm:w-6 text-primary-foreground" /> */}
               </div>
-              <div className="min-w-0">
-{/*            <h1 className="text-lg sm:text-xl font-bold text-primary truncate flex items-center">
-                  Chieta Desk System
-                </h1>
-                <p className="text-xs sm:text-sm text-muted-foreground hidden sm:block">Administration Dashboard</p> */}
-              </div> 
             </div>
 
             <div className="flex items-center space-x-2 sm:space-x-4">
